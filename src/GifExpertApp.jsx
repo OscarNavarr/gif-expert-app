f334423f5b8ee04ee93539f2cec085d48bfb8825
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2022-08-13 08:19:58 +0200

    First Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,32 @@
+import {useState} from "react";
+import {AddCategory} from './components/AddCategory';
+import { GifGrid } from "./components/GifGrid";
+
+export default function GifExpertApp() {
+
+  const [categories, setCategories] = useState(['Oscar Navarro'])
+  
+  const onAddCategory = (newCategory) => {
+
+    if(categories.includes(newCategory)) return;
+
+    setCategories([ newCategory, ...categories]);
+
+  } 
+
+  return (
+    <>
+        <h1>Gif Expert App</h1>
+        <AddCategory 
+           onNewCategory={ (event) => onAddCategory(event)}
+          currentCategories = {categories} />
+                
+        {
+          categories.map( category => (
+            <GifGrid key={category} category={category}/>
+          ))
+        }        
+        
+    </>
+  )
+}
